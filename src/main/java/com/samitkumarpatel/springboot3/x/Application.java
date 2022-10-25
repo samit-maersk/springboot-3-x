1f50c7cefba0f5141a35db80fd4d6da4397b3d22
samit-maersk <SamitKumar.Patel@maersk.com>
2022-10-25 11:18:07 +0200

    pipeline improvement

--- src/main/java/com/samitkumarpatel/springboot3/x/Application.java
+++ src/main/java/com/samitkumarpatel/springboot3/x/Application.java
@@ -2,6 +2,15 @@ package com.samitkumarpatel.springboot3.x;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +19,11 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+}
+@Configuration
+class Router {
+	@Bean
+	public RouterFunction route() {
+		return RouterFunctions.route().GET("/",accept(MediaType.TEXT_PLAIN), request -> ServerResponse.ok().body(Mono.just("Hello World"), String.class)).build();
+	}
 }
